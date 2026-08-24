54f07208d062ad44c386c4b330288feefb44dfae
pedrobernardes03 <pedro17022003@gmail.com>
2026-08-24 19:40:58 -0300

    meu primeiro commit

---
+++ aula5/user.js
@@ -0,0 +1,57 @@
+var users = JSON.parse(localStorage.getItem("users")) || [] // parse: reverte de string para objeto 
+
+var logado = JSON.parse(localStorage.getItem("logado")) || {}
+var welcome = document.getElementById("welcome")
+if(welcome && logado) welcome.innerHTML = "Olá " + logado.nome
+
+var logUsers = document.getElementById("logUsers")
+if(logUsers)
+    logUsers.innerHTML = JSON.stringify(users,null,4)
+
+
+var formR = document.getElementById("formRegister");
+formR?.addEventListener("submit", (e) => { // Listener reconhece que o button foi acionado 
+// "formR?" = if(formL)    
+    e.preventDefault(); //impede atualizar a tela
+
+    let name = document.getElementById("iName").value
+    let email = document.getElementById("iEmail").value
+    let password = document.getElementById("iPassword").value
+    let date = document.getElementById("iDate").value
+
+    const user = { //objeto anônimo, estrutura, json
+        nome: name,
+        email: email,
+        senha: password,
+        nascimento: date
+    }
+    users.push(user)
+    localStorage.setItem("users", JSON.stringify(users)) // stringfy: transforma em texto (string)
+    
+})
+
+var formL = document.getElementById("formLogin")
+if(formL)formL.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let email = document.getElementById("lEmail").value
+    let password = document.getElementById("lPassword").value
+
+    let user = users.find(u => {
+        return u.email == email
+    })
+
+    if (!user) { // not usuario
+        console.log("usuario não encontrado")
+        return
+    }
+
+    if (user.senha == password) {
+        console.log("usuario logado")
+        localStorage.setItem("logado", JSON.stringify(user))
+        window.location.href = "painel.html"
+        
+    } else {
+        console.log("senha invalida")
+    }
+})
